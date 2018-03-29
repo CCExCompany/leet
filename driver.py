1e7a54fd8e484fe68f98561d63d7182787d1fcdc
Vladimir Miloserdov <milosvova@gmail.com>
2018-03-29 04:09:36 +0300

    Better file structure and setup.py

--- driver.py
+++ driver.py
@@ -4,6 +4,9 @@
 
 import os
 import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+
 import curses
 import logging
 from ui import MainWindow
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='leet',
+    version=1.0,
+    author='CCEx',
+    description='Encryption tool',
+    license='GPLv3'
+)
